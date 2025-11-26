2540201fae24099ca701907666ee1c17513c585d
sakilalakmal <sakilalakmal77@gmail.com>
2025-11-26 19:45:49 +0530

    feat: enhance job filtering by adding jobTypes support in JobFilter and JobListingView

--- app/(mainLayout)/page.tsx
+++ app/(mainLayout)/page.tsx
@@ -6,6 +6,7 @@ import { Suspense } from "react";
 type SearchParams = {
   searchParams: Promise<{
     page?: string;
+    jobTypes?: string;
   }>;
 };
 
@@ -14,12 +15,14 @@ export default async function Home({ searchParams }: SearchParams) {
 
   const currentPage = Number(params.page) || 1;
 
+  const jobTypes = params.jobTypes?.split(",") || [];
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-8">
       <JobFilter />
       <div className="col-span-2 flex flex-col gap-6">
         <Suspense fallback={<JobListingSkeleton />} key={currentPage}>
-          <JobListingView currentPage={currentPage} />
+          <JobListingView currentPage={currentPage} jobTypes={jobTypes} />
         </Suspense>
       </div>
     </div>
--- components/general/JobFilter.tsx
+++ components/general/JobFilter.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { X } from "lucide-react";
 import { Button } from "../ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
@@ -14,15 +15,59 @@ import {
   SelectValue,
 } from "../ui/select";
 import { countryList } from "@/app/utils/coutries-list";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
 
 const jobTypes = ["full-time", "part-time", "contract", "internship"];
 
 export function JobFilter() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  // get current filters from url
+  const currentJobTypes = searchParams.get("jobTypes")?.split(",") || [];
+
+  function clearAllFilters() {
+    router.push("/");
+  }
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      if (value) {
+        params.set(name, value);
+      } else {
+        params.delete(name);
+      }
+      return params.toString();
+    },
+    [searchParams]
+  );
+
+  function handleJobTypeChange(jobType: string, checked: boolean) {
+    const current = new Set(currentJobTypes);
+
+    if (checked) {
+      current.add(jobType);
+    } else {
+      current.delete(jobType);
+    }
+
+    const newValue = Array.from(current).join(",");
+
+    router.push(`?${createQueryString("jobTypes", newValue)}`);
+  }
+
   return (
     <Card className="col-span-1 h-fit">
       <CardHeader className="flex flex-wrap justify-between items-center">
         <CardTitle className="text-2xl font-semibold">Filter Jobs</CardTitle>
-        <Button variant={"destructive"} size={"sm"} className="h-8">
+        <Button
+          variant={"destructive"}
+          size={"sm"}
+          className="h-8"
+          onClick={clearAllFilters}
+        >
           <span>clear filters</span>
           <X className="size-4" />
         </Button>
@@ -36,7 +81,13 @@ export function JobFilter() {
           <div className="grid grid-cols-2 gap-4">
             {jobTypes.map((job, index) => (
               <div key={index} className="flex items-center space-x-2">
-                <Checkbox id={job} />
+                <Checkbox
+                  onCheckedChange={(checked) => {
+                    handleJobTypeChange(job, checked as boolean);
+                  }}
+                  id={job}
+                  checked={currentJobTypes.includes(job)}
+                />
                 <Label className="text-sm font-medium" htmlFor={job}>
                   {job}
                 </Label>
